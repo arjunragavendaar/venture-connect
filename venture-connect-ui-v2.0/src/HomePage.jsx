839a04566bee9f7b81dd94cbb446f1ba300a5245
Arjun <35588227+arjunragavendaar@users.noreply.github.com>
2023-12-31 17:40:10 -0500

    Venture Connect feed changes

---
+++ venture-connect-ui-v2.0/src/HomePage.jsx
@@ -0,0 +1,24 @@
+import {useState} from 'react';
+import React from 'react';
+import axios from 'axios';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Stack from 'react-bootstrap/Stack';
+import Layout from '../src/components/Layout';
+import Entrepreneurform from '../src/components/Entrepreneurform';
+import Technocratform from '../src/components/Technocratform';
+import Investorform from '../src/components/Investorform';
+import Companyform from '../src/components/Companyform';
+import Managementform from '../src/components/Managementform';
+import ProfileData from '../src/data_models/profiledata';
+ 
+export default function  Homepage()  {
+return  (
+  <Layout>
+
+  <div>
+
+
+  </div>
+  </Layout>
+)};
