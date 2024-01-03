ce0f60e3ff560078cf1bcf97cc4af237f1942e67
Arjun <35588227+arjunragavendaar@users.noreply.github.com>
2024-01-02 20:41:32 -0500

    app.js commit changes

--- venture-connect-ui-v2.0/pages/_app.js
+++ venture-connect-ui-v2.0/pages/_app.js
@@ -1,5 +1,10 @@
 import '../styles/home.css'
- 
+ import'../styles/venturehome.css'
+ import'../styles/feedoptions.css'
+ import'../styles/postcomponent.css'
+ import'../styles/leftprofcomp.css'
+ import '../styles/rightcomp.css'
+
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
